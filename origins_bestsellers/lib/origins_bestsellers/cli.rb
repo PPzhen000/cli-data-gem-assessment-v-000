77ad92997be43beec28c82f368e89e1b3b3be4fd
Ariel Zhen <zhenpingping@live.com>
2018-06-30 04:58:55 +0000

    display_products

--- origins_bestsellers/lib/origins_bestsellers/cli.rb
+++ origins_bestsellers/lib/origins_bestsellers/cli.rb
@@ -8,19 +8,33 @@ require 'open-uri'
 class CLI
 
   def run
-
-  end
-
-  def display_products
     puts "You are viewing the best sellers of Origins"
     puts "Please view the bestsellers of Origins from 3 catagories: 1.SKINCARE, 2.BATH & BODY, 3.MAKEUP"
     puts "Which catagory would you like to see the list of best sellers? Please type in the corresponding number for each catagory or type exit:"
     input = gets.strip
 
+    case input
+    when "1"
+      # Scraper.scrape_skincare_page
+    when "2"
+      # Scraper.scrape_bath_and_body_page
+    when "3"
+      # Scraper.scrape_makeup_page
+    when "exit"
+      goodbye
+    end
   end
 
   def goodbye
-    puts "Please come back to see our updates on bestsellers"
+    puts "Please come back to see our updates on bestsellers!"
   end
+
+
+  def display_products
+    Pruduct.all.each.with_index(1) do |product, index|
+      puts "#{index} - #{product.name} - #{product.price}"
+    end
+  end
+
 
 end
