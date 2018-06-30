8906fffe281fcd828695275ee29009906cb7655d
Ariel Zhen <zhenpingping@live.com>
2018-06-30 04:36:49 +0000

    starting cli

--- origins_bestsellers/lib/origins_bestsellers/cli.rb
+++ origins_bestsellers/lib/origins_bestsellers/cli.rb
@@ -1,4 +1,11 @@
-class OriginsBestsellers::CLI
+require_relative '../lib/origins_bestsellers/scraper'
+require_relative '../lib/origins_bestsellers/product'
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+class CLI
   def run
     Scraper.list_menu()
   end
