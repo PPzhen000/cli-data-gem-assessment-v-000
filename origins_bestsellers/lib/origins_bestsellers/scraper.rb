8728050d6b01e157b9e61e05cebe09b1daae0203
Ariel Zhen <zhenpingping@live.com>
2018-06-30 00:01:00 +0000

    fix scraper method

--- origins_bestsellers/lib/origins_bestsellers/scraper.rb
+++ origins_bestsellers/lib/origins_bestsellers/scraper.rb
@@ -4,21 +4,36 @@ require 'open-uri'
 
 # class OriginsBestsellers::Scraper
 class Scraper
+
   def self.scrape_skincare_page
 
     skincare_page = Nokogiri::HTML(open("https://www.origins.com/skincare-bestsellers"))
 
     skincare_product = []
+  #   skincare_page.css(".product-grid-wrapper").each do |product|
+   #
+  #     product_name = product.css(".product-brief__subtitle").text.strip
+  #     product_price = product.css(".product-brief__price.down-price").text.strip
+  #   # links_to_buy = product.css(".product-brief__subtitle__link").attr("href").text
+   #
+  #   #  links_to_buy.each do |link|
+  #   #    puts "product link = " + link.text
+  #   #  end
+  #   # skincare_product << {name: product_name, price: product_price, product_link: links_to_buy}
+  #     skincare_product = {name: product_name, price: product_price}
+   #
+  #  end
 
-    product_name = skincare_page.css(".product-brief__subtitle").text
-    product_price = skincare_page.css(".product-brief__price.down-price").text.strip
-    #number_of_reviews = skincare_page.css("span.pr-snippet-review-count").text
-    links_to_buy = skincare_page.css(".product-brief__subtitle__link").attribute("href")
+  count = 0
+  while skincare_page.css(".product-brief__subtitle").size > count do
+    product_name = skincare_page.css(".product-brief__subtitle")[count].text.strip
+    product_price = skincare_page.css(".product-brief__price.down-price")[count].text.strip
+    skincare_product[count] = {name: product_name, price: product_price}
+    count +=1
+  end
 
-     links_to_buy.each do |link|
-       puts "product link = " + link.text
-     end
-     binding.pry
+   skincare_product
+    binding.pry
   end
 
   def self.scrape_bath_and_body_page
